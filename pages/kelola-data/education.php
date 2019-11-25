f4178ea8871d05616df043ec9cfe842938e112e0
varrielnzr39 <muhvarriel@gmail.com>
2019-11-25 21:30:55 +0700

    yang education dan skill2

--- pages/kelola-data/education.php
+++ pages/kelola-data/education.php
@@ -61,7 +61,7 @@
           <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-            <h4 class="modal-title">Edit Education</h4>
+            <h4 class="modal-title">Add Education</h4>
           </div>
           <div class="modal-body">
 
