8450a285c2b57a9c24a3004b5f17d6d7880a5008
stetlar <iatmanegara.22@gmail.com>
2019-11-27 21:53:44 +0700

    Update training

--- pages/kelola-data/training-workshop-seminar.php
+++ pages/kelola-data/training-workshop-seminar.php
@@ -1,182 +1,142 @@
 <?php include '../static/top.php'; ?>
-    <section class="content">
+  <section class="content">
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Personal Details</h3>
+              <h3 class="box-title">List Education</h3>
+              <div class="pull-right box-tools">
+                <button class="btn btn-primary btn-md" data-toggle="modal" data-target="#modalAdd" title="" data-original-title="Remove"><i class="fa fa-plus"> </i> Add Education</button>
+            </div>
             </div><!-- /.box-header -->
             <div class="box-body">
-              <div class="form-group">
-                <label>NIM</label>
-                <input type="text" class="form-control" name="nim" placeholder="Masukan NIM">
-              </div>
-              <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" name="name" placeholder="Masukan Name">
-              </div>
-              <div class="form-group">
-                <label>Tempat Lahir</label>
-                <input type="text" class="form-control" name="nama_jurnal" placeholder="Masukan Tempat Lahir">
-              </div>
-              <div class="form-group">
-                <label>Tanggal Lahir</label>
-                <div class="input-group date">
-                  <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                  </div>
-                  <input type="text" class="form-control pull-right" id="datepicker">
-                </div>
-              </div>
-               <div class="form-group">
-                <label>No</label>
-                <input type="text" class="form-control" name="No" placeholder="Masukan No">
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input type="text" class="form-control" name="Email" placeholder="Masukan Email">
-              </div>
-              <div class="form-group">
-                <label>Alamat</label>
-                <input type="text" class="form-control" name="alamat" placeholder="Masukan Alamat">
-              </div>
-              <br>
-              <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Status</label>
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Nama</th>
+                    <th></th>
+                    <th>Posisi</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                        <td>Amazing You</td>
+                        <td></td>
+                        <td>Panitia</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
 
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Status" placeholder="Masukan Status">
-                  </div>
-              </div>
-              <br>
-              <br>
-               <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Prodi</label>
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                  </tr>
+                  <tr>
+                        <td>Github Workshop</td>
+                        <td></td>
+                        <td>Peserta</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
 
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Prodi" placeholder="Masukan Prodi">
-                  </div>
-              </div>
-              <br>
-              <br>
-               <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Angkatan</label>
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                  </tr>
+                  <tr>
+                        <td>Techtalk</td>
+                        <td></td>
+                        <td>Panitia</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
 
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Angkatan" placeholder="Masukan Angkatan">
-                  </div>
-              </div>
-              <br>
-              <br>
-              <div class="form-group mt-5">
-                <label>Media Sosial</label>
-                <input type="text" class="form-control" name="Media Sosial" placeholder="Masukan Media Sosial">
-              </div>
-
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox"> Check me out
-                </label>
-              </div>
-              
-              <div class="box-footer">
-                <button type="button" class="btn btn-primary">Simpan</button>
-              </div>
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                  </tr>
+                </tbody>
+              </table>
             </div><!-- /.box-body -->
           </div><!-- /.box -->
         </div><!-- /.col -->
 
-      </div><!-- /.row -->
-    </section>
-<?php include '../static/bot.php'; ?>
-<?php include '../static/top.php'; ?>
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Personal Details</h3>
-            </div><!-- /.box-header -->
-            <div class="box-body">
-              <div class="form-group">
-                <label>NIM</label>
-                <input type="text" class="form-control" name="nim" placeholder="Masukan NIM">
-              </div>
-              <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" name="name" placeholder="Masukan Name">
-              </div>
-              <div class="form-group">
-                <label>Tempat Lahir</label>
-                <input type="text" class="form-control" name="nama_jurnal" placeholder="Masukan Tempat Lahir">
-              </div>
-              <div class="form-group">
-                <label>Tanggal Lahir</label>
-                <div class="input-group date">
-                  <div class="input-group-addon">
-                    <i class="fa fa-calendar"></i>
-                  </div>
-                  <input type="text" class="form-control pull-right" id="datepicker">
-                </div>
-              </div>
-               <div class="form-group">
-                <label>No</label>
-                <input type="text" class="form-control" name="No" placeholder="Masukan No">
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input type="text" class="form-control" name="Email" placeholder="Masukan Email">
-              </div>
-              <div class="form-group">
-                <label>Alamat</label>
-                <input type="text" class="form-control" name="alamat" placeholder="Masukan Alamat">
-              </div>
-              <br>
-              <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Status</label>
+     <div id="modalAdd" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+          <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="modal-title">Add Education</h4>
+          </div>
+          <div class="modal-body">
 
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Status" placeholder="Masukan Status">
-                  </div>
-              </div>
-              <br>
-              <br>
-               <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Prodi</label>
-
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Prodi" placeholder="Masukan Prodi">
-                  </div>
-              </div>
-              <br>
-              <br>
-               <div class="form-group mt-5">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Angkatan</label>
-
-                  <div class="col-sm-10">
-                  <input type="text" class="form-control" name="Angkatan" placeholder="Masukan Angkatan">
-                  </div>
-              </div>
-              <br>
-              <br>
-              <div class="form-group mt-5">
-                <label>Media Sosial</label>
-                <input type="text" class="form-control" name="Media Sosial" placeholder="Masukan Media Sosial">
+             <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
               </div>
 
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox"> Check me out
-                </label>
+              <div class="form-group">
+                <label>Tahun</label>
+                <input type="text" class="form-control" name="tahun" placeholder="Masukan Tahun">
               </div>
-              
-              <div class="box-footer">
-                <button type="button" class="btn btn-primary">Simpan</button>
+
+              <div class="form-group">
+                <label>Kota</label>
+                <input type="text" class="form-control" name="kota" placeholder="Masukan Kota">
               </div>
-            </div><!-- /.box-body -->
-          </div><!-- /.box -->
-        </div><!-- /.col -->
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
+            <button type="button" class="btn btn-primary">Simpan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="modalEdit" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+          <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="modal-title">Edit Education</h4>
+          </div>
+          <div class="modal-body">
+             <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
+              </div>
+
+              <div class="form-group">
+                <label>Tahun</label>
+                <input type="text" class="form-control" name="tahun" placeholder="Masukan Tahun">
+              </div>
+
+              <div class="form-group">
+                <label>Kota</label>
+                <input type="text" class="form-control" name="kota" placeholder="Masukan Kota">
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
+            <button type="button" class="btn btn-primary">Simpan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="modalDelete" class="modal modal-danger fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <h4 class="modal-title">Delete Education</h4>
+            </div>
+            <div class="modal-body">
+              <p>Apakah kamu ingin menghapus data ini?</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Tutup</button>
+              <button type="button" class="btn btn-outline">YA</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
       </div><!-- /.row -->
-    </section>
+  </section>
 <?php include '../static/bot.php'; ?>
