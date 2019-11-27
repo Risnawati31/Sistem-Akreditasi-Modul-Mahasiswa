464196d8a64bb59fa8a11283ecf1a6744ff924b0
stetlar <iatmanegara.22@gmail.com>
2019-11-27 19:24:44 +0700

    menyesuaikan tampilan

--- pages/kelola-data/training-workshop-seminar.php
+++ pages/kelola-data/training-workshop-seminar.php
@@ -264,29 +264,29 @@ $(document).ready(function(){
             <div class="table-title">
                 <div class="row">
                     <div class="col-sm-6">
-            <h2>Manage <b>Employees</b></h2>
+            <h2>List <b>Training, Workshop, Seminar</b></h2>
           </div>
           <div class="col-sm-6">
-            <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+            <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New</span></a>
             <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>            
           </div>
                 </div>
             </div>
             <table class="table table-striped table-hover">
                 <thead>
-                    <tr>
+                    <tr> //start new slot
             <th>
               <span class="custom-checkbox">
                 <input type="checkbox" id="selectAll">
                 <label for="selectAll"></label>
               </span>
             </th>
-                        <th>Name</th>
-                        <th>Email</th>
-            <th>Address</th>
-                        <th>Phone</th>
-                        <th>Actions</th>
-                    </tr>
+                        <th>Nama Kegiatan</th>
+                        <th> </th>
+            <th> </th>
+                        <th>Posisi</th>
+                        <th> </th>
+                    </tr> //end slot
                 </thead>
                 <tbody>
                     <tr>
@@ -296,10 +296,10 @@ $(document).ready(function(){
                 <label for="checkbox1"></label>
               </span>
             </td>
-                        <td>Thomas Hardy</td>
-                        <td>thomashardy@mail.com</td>
-            <td>89 Chiaroscuro Rd, Portland, USA</td>
-                        <td>(171) 555-2222</td>
+                        <td>Amazing You</td>
+                        <td> </td>
+            <td> </td>
+                        <td>Panitia</td>
                         <td>
                             <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                             <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
@@ -312,10 +312,10 @@ $(document).ready(function(){
                 <label for="checkbox2"></label>
               </span>
             </td>
-                        <td>Dominique Perrier</td>
-                        <td>dominiqueperrier@mail.com</td>
-            <td>Obere Str. 57, Berlin, Germany</td>
-                        <td>(313) 555-5735</td>
+                        <td>Github Workshop</td>
+                        <td></td>
+            <td></td>
+                        <td>Peserta</td>
                         <td>
                             <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                             <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
@@ -328,47 +328,16 @@ $(document).ready(function(){
                 <label for="checkbox3"></label>
               </span>
             </td>
-                        <td>Maria Anders</td>
-                        <td>mariaanders@mail.com</td>
-            <td>25, rue Lauriston, Paris, France</td>
-                        <td>(503) 555-9931</td>
+                        <td>TechTalk</td>
+                        <td></td>
+            <td></td>
+                        <td>Panitia</td>
                         <td>
                             <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
                             <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
                         </td>
                     </tr>
-                    <tr>
-            <td>
-              <span class="custom-checkbox">
-                <input type="checkbox" id="checkbox4" name="options[]" value="1">
-                <label for="checkbox4"></label>
-              </span>
-            </td>
-                        <td>Fran Wilson</td>
-                        <td>franwilson@mail.com</td>
-            <td>C/ Araquil, 67, Madrid, Spain</td>
-                        <td>(204) 619-5731</td>
-                        <td>
-                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                        </td>
-                    </tr>         
-          <tr>
-            <td>
-              <span class="custom-checkbox">
-                <input type="checkbox" id="checkbox5" name="options[]" value="1">
-                <label for="checkbox5"></label>
-              </span>
-            </td>
-                        <td>Martin Blank</td>
-                        <td>martinblank@mail.com</td>
-            <td>Via Monte Bianco 34, Turin, Italy</td>
-                        <td>(480) 631-2097</td>
-                        <td>
-                            <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                        </td>
-                    </tr> 
+                     
                 </tbody>
             </table>
       <div class="clearfix">
