1af7573a6bf190c2fd34be741b11273b6fba4ea3
ZulfikriMaulana <fikri1838@gmail.com>
2019-11-28 08:07:44 +0700

    Menambahkan Working Internship

--- pages/kelola-data/working-internship.php
+++ pages/kelola-data/working-internship.php
@@ -1,5 +1,128 @@
 <?php include '../static/top.php'; ?>
-  <section class="content">
-    ISINYA DISINI YA
-  </section>
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Working Internship</h3>
+              <div class="pull-right box-tools">
+	            	<button class="btn btn-primary btn-md" data-toggle="modal" data-target="#modalAdd" title="" data-original-title="Remove"><i class="fa fa-plus"> </i> Add Project</button>
+	          </div>
+            </div><!-- /.box-header -->
+            <div class="box-body">
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>Nama</th>
+                    <th>Tahun</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                	<tr>
+                        <td>Sistem Akreditasi</td>
+                        <td>2018</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
+
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                	</tr>
+                	<tr>
+                        <td>Sistem Akreditasi</td>
+                        <td>2018</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
+
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                	</tr>
+                	<tr>
+                        <td>Sistem Akreditasi</td>
+                        <td>2018</td>
+                        <td>
+                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
+
+                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
+                        </td>
+                	</tr>
+                </tbody>
+              </table>
+            </div><!-- /.box-body -->
+          </div><!-- /.box -->
+        </div><!-- /.col -->
+
+     <div id="modalAdd" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+          <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <h4 class="modal-title">Edit Project</h4>
+          </div>
+          <div class="modal-body">
+
+             <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
+              </div>
+
+              <div class="form-group">
+                <label>Tahun</label>
+                <textarea type="text" class="form-control" name="alamat" placeholder="Masukan Tahun"></textarea>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
+            <button type="button" class="btn btn-primary">Simpan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="modalEdit" class="modal fade" role="dialog">
+  		<div class="modal-dialog">
+	        <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+		        <h4 class="modal-title">Edit Project</h4>
+		      </div>
+		      <div class="modal-body">
+             <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
+              </div>
+
+              <div class="form-group">
+                <label>Tahun</label>
+                <textarea type="text" class="form-control" name="alamat" placeholder="Masukan Tahun"></textarea>
+              </div>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
+		        <button type="button" class="btn btn-primary">Simpan</button>
+		      </div>
+		    </div>
+      </div>
+    </div>
+
+		<div id="modalDelete" class="modal modal-danger fade" role="dialog">
+	  		<div class="modal-dialog">
+		        <div class="modal-content">
+			      <div class="modal-header">
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+			        <h4 class="modal-title">Delete Project</h4>
+			      </div>
+			      <div class="modal-body">
+			        <p>Apakah kamu ingin menghapus data ini?</p>
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Tutup</button>
+			        <button type="button" class="btn btn-outline">YA</button>
+			      </div>
+			    </div>
+		    </div>
+	    </div>
+
+      </div><!-- /.row -->
+    </section>
 <?php include '../static/bot.php'; ?>
