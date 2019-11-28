fe06a654d9702c66b5b94ed35dbbffad47ec1bab
ZulfikriMaulana <fikri1838@gmail.com>
2019-11-28 23:29:42 +0700

    Update Working

--- pages/kelola-data/working-internship.php
+++ pages/kelola-data/working-internship.php
@@ -5,138 +5,48 @@
           <div class="box">
             <div class="box-header">
               <h3 class="box-title">Working Internship</h3>
-              <div class="pull-right box-tools">
-	            	<button class="btn btn-primary btn-md" data-toggle="modal" data-target="#modalAdd" title="" data-original-title="Remove"><i class="fa fa-plus"> </i> Add Project</button>
-	          </div>
             </div><!-- /.box-header -->
             <div class="box-body">
-              <table class="table table-bordered table-striped">
-                <thead>
-                  <tr>
-                    <td>ID</td>
-                    <td>Nama</td>
-                    <td>Posisi</td>
-                    <td>Tahun Mulai</td>
-                    <td>Tahun Berakhir</td>
-                    <td>Deskripsi</td>
-                  </tr>
-                </thead>
-                <tbody>
-                	<tr>
-                    <td>01</td>
-                    <td>Isi Nama</td>
-                    <td>Isi Posisi</td>
-                    <td>Isi Tahun Mulai</td>
-                    <td>Isi Tahun Berakhir</td>
-                    <td>Isi Deskripsi</td>
-                        <td>
-                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
-
-                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
-                        </td>
-                	</tr>
-                	<tr>
-                    <td>02</td>
-                    <td>Isi Nama</td>
-                    <td>Isi Posisi</td>
-                    <td>Isi Tahun Mulai</td>
-                    <td>Isi Tahun Berakhir</td>
-                    <td>Isi Deskripsi</td>
-                        <td>
-                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
-
-                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
-                        </td>
-                	</tr>
-                	<tr>
-                    <td>03</td>
-                    <td>Isi Nama</td>
-                    <td>Isi Posisi</td>
-                    <td>Isi Tahun Mulai</td>
-                    <td>Isi Tahun Berakhir</td>
-                    <td>Isi Deskripsi</td>
-                        <td>
-                            <a href="#editPersonalDetail" class="btn  btn-warning btn-md" data-toggle="modal" data-target="#modalEdit"><i class="fa fa-edit" data-toggle="tooltip" title="Edit"></i> Edit</a>
-
-                            <a href="#deletePersonalDetail" class="btn  btn-danger btn-md" data-toggle="modal" data-target="#modalDelete"><i class="fa fa-trash" data-toggle="tooltip" title="Delete"></i> Delete</a>
-                        </td>
-                	</tr>
-                </tbody>
-              </table>
+              <div class="form-group">
+                <label>ID</label>
+                <input type="text" class="form-control" name="nim" placeholder="Masukan ID">
+              </div>
+              <div class="form-group">
+                <label>Nama</label>
+                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
+              </div>
+              <div class="form-group">
+                <label>Posisi</label>
+                <input type="text" class="form-control" name="nama_jurnal" placeholder="Masukan Posisi">
+              </div>
+              <div class="form-group">
+                <label>Tahun Mulai Lahir</label>
+                <div class="input-group date">
+                  <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                  </div>
+                  <input type="text" class="form-control pull-right" id="datepicker">
+                </div>
+              </div>
+              <div class="form-group">
+                <label>Tahun Berakhir</label>
+                <div class="input-group date">
+                  <div class="input-group-addon">
+                    <i class="fa fa-calendar"></i>
+                  </div>
+                  <input type="text" class="form-control pull-right" id="datepicker">
+                </div>
+              </div>
+               <div class="form-group">
+                <label>Deskripsi</label>
+                <input type="text" class="form-control" name="No" placeholder="Masukan Deskripsi">
+              </div>              
+              <div class="box-footer">
+                <button type="button" class="btn btn-primary">Simpan</button>
+              </div>
             </div><!-- /.box-body -->
           </div><!-- /.box -->
         </div><!-- /.col -->
-
-     <div id="modalAdd" class="modal fade" role="dialog">
-      <div class="modal-dialog">
-          <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-            <h4 class="modal-title">Edit Project</h4>
-          </div>
-          <div class="modal-body">
-
-             <div class="form-group">
-                <label>Nama</label>
-                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
-              </div>
-
-              <div class="form-group">
-                <label>Tahun</label>
-                <textarea type="text" class="form-control" name="alamat" placeholder="Masukan Tahun"></textarea>
-              </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
-            <button type="button" class="btn btn-primary">Simpan</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="modalEdit" class="modal fade" role="dialog">
-  		<div class="modal-dialog">
-	        <div class="modal-content">
-		      <div class="modal-header">
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-		        <h4 class="modal-title">Edit Project</h4>
-		      </div>
-		      <div class="modal-body">
-             <div class="form-group">
-                <label>Nama</label>
-                <input type="text" class="form-control" name="name" placeholder="Masukan Nama">
-              </div>
-
-              <div class="form-group">
-                <label>Tahun</label>
-                <textarea type="text" class="form-control" name="alamat" placeholder="Masukan Tahun"></textarea>
-              </div>
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tutup</button>
-		        <button type="button" class="btn btn-primary">Simpan</button>
-		      </div>
-		    </div>
-      </div>
-    </div>
-
-		<div id="modalDelete" class="modal modal-danger fade" role="dialog">
-	  		<div class="modal-dialog">
-		        <div class="modal-content">
-			      <div class="modal-header">
-			        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-			        <h4 class="modal-title">Delete Project</h4>
-			      </div>
-			      <div class="modal-body">
-			        <p>Apakah kamu ingin menghapus data ini?</p>
-			      </div>
-			      <div class="modal-footer">
-			        <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Tutup</button>
-			        <button type="button" class="btn btn-outline">YA</button>
-			      </div>
-			    </div>
-		    </div>
-	    </div>
 
       </div><!-- /.row -->
     </section>
